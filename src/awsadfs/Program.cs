a7a04a076b3774d4e130f636d18d75ecd509617d
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2022-12-12 02:05:03 -0800

    add program.cs and basic scaffolding of the commands

--- src/awsadfs/Program.cs
+++ src/awsadfs/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace awsadfs
+﻿using System;
+using System.CommandLine;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace awsadfs
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Define root  command
+            var rootCommand = new RootCommand("command line tool to ease AWS cli" +
+                " authentication against ADFS (multi factor authentication with active directory).");
+            #endregion
+
+            #region Define Login subcommand and its options
+            var loginSubcommand = new Command("login", "login to aws using adfs");
+
+            var adfsHostOption = new Option<string>("--adfs-host", "your-adfs-hostname");
+            var profileOption = new Option<string>("--profile", "your aws profile");
+            var authfileOption = new Option<string>("--authfile", "/path/and/file/name");
+            #endregion
+            //add options to the loginSubcommand
+            loginSubcommand.AddOption(adfsHostOption);
+            loginSubcommand.AddOption(profileOption);
+            loginSubcommand.AddOption(authfileOption); 
+
+            //Add loginSubcommand to rootCommand
+            rootCommand.Add(loginSubcommand);
+
+
+
+
+
+            return await rootCommand.InvokeAsync(args);
         }
+                
     }
 }
